4ebce588d441b1b856c25cfe11c33bd8142c0ccc
holo <1799364931@qq.com>
2025-12-23 21:58:05 +0800

    add:增加用户态日志获取功能

--- src/usr/logger/logger.cpp
+++ src/usr/logger/logger.cpp
@@ -1,5 +1,7 @@
 
+#include <chrono>
 #include <iostream>
+#include <thread>
 #include "../controller/netlink_tool/netlink_tool.h"
 
 void check_ret(int ret) {
@@ -18,8 +20,35 @@ int main() {
     netlink_tool.send_buffer("", 0, CMD_LOGGING_REGISTER, ATTR_BUF);
     // 后面一直接受即可
 
-    while (true) {
-        ret = (int)netlink_tool.recv_once();
-        check_ret(ret);
-    }
+    // auto recv_func = [&] {
+    //     while (true) {
+    //         // std::this_thread::sleep_for(std::chrono::seconds(1));
+    //         ret = (int)netlink_tool.recv_reply_once();
+    //         check_ret(ret);
+    //     }
+    // };
+
+    std::thread recv_thread([&] {
+        while (true) {
+            // std::this_thread::sleep_for(std::chrono::seconds(1));
+            ret = (int)netlink_tool.recv_reply_once();
+            check_ret(ret);
+        }
+    });
+
+    std::thread print_thread([&] {
+        while (true) {
+            std::vector<std::string> logs;
+            netlink_tool.get_log_info_queue().fetch_logs(logs);
+            for (auto& log : logs) {
+                std::cout << log << std::endl;
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(10));
+        }
+    });
+
+    recv_thread.join();
+    print_thread.join();
+
+
 }
